988474063e5269bee02e8988e1998fc1b5df178c
Lore-ai2106 <20251ti0008@ifba.edu.br>
2026-03-03 11:53:05 -0300

    Update TV.java

--- TV.java
+++ TV.java
@@ -1,1 +1,36 @@
+Class TV {
+  int tamanho;
+  int volume;
+  String marca;
+  int voltagem;
+  int canal;
+  int consumo;
 
+  void ligar () {
+    consumo = tamanho * voltagem;
+    IO.print("Consumo é: " + consumo);
+  }
+  void desligar() {
+
+  }
+  void volumeaumentar() {
+    if(volume<10){
+      volume++;
+    }
+    
+  }
+  void volumediminuir() {
+    if(volume>1){
+      volume--;
+    }
+    
+  }
+  void canalmais() {
+    canal++;
+  }
+  void canalmenos() {
+    canal--;
+  }
+
+}
+    
